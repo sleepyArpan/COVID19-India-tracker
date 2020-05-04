6a847e7af85c5412d35a297b412bec9c9ef7721c
sleepyarpan <arpanchattopadhyay23@gmail.com>
2020-05-04 21:48:39 +0530

    Data destructured for national API call

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,12 @@ const url = 'https://akashraj.tech/corona/api_india';
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(`${url}`);
-    return response;
+    const {
+      data: {
+        total_values: { confirmed, deaths, recovered, lastupdatedtime },
+      },
+    } = await axios.get(`${url}`);
+    return { confirmed, deaths, recovered, lastupdatedtime };
   } catch (error) {
     console.log(error);
   }
